57b0f9659e512fe21d7b09d87c6162f0d3e13856
fake-dp <fakecoko92@gmail.com>
2023-02-10 09:29:05 +0900

    수정 기능 완료

--- client/src/components/todo/TodoItem.jsx
+++ client/src/components/todo/TodoItem.jsx
@@ -1,39 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import todoAPI from "../../api/todo";
 
 function TodoItem({ todos, setTodos, item }) {
+  const [modifyToggle, setModifyToggle] = useState(false);
+  const [todoUpdateText, setTodoUpdateText] = useState("");
+
+  const textOnChange = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setTodoUpdateText(value);
+  };
+
   const deleteTodoList = async (id) => {
     await todoAPI.deleteTodo(id);
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   const updateTodoList = async (id, isCompleted) => {
-    const data = await todoAPI.updateTodo(id, "변경test", isCompleted);
+    setModifyToggle(false);
+    const data = await todoAPI.updateTodo(id, todoUpdateText, isCompleted);
     setTodos(todos.map((todo) => (todo.id === id ? data : todo)));
   };
 
   return (
-    <li>
-      <label>
-        <input type="checkbox" />
-        <span>{item.todo}</span>
-      </label>
+    <div>
+      {modifyToggle ? (
+        <li>
+          <input onChange={textOnChange} />
 
-      <>
-        <button
-          onClick={() => updateTodoList(item.id, item.isCompleted)}
-          data-testid="modify-button"
-        >
-          수정
-        </button>
-        <button
-          onClick={() => deleteTodoList(item.id)}
-          data-testid="delete-button"
-        >
-          삭제
-        </button>
-      </>
-    </li>
+          <button
+            onClick={() => updateTodoList(item.id, item.isCompleted)}
+            data-testid="modify-button"
+          >
+            완료
+          </button>
+          <button
+            onClick={() => deleteTodoList(item.id)}
+            data-testid="delete-button"
+          >
+            삭제
+          </button>
+        </li>
+      ) : (
+        <li>
+          <label>
+            <input type="checkbox" />
+            <span>{item.todo}</span>
+          </label>
+
+          <button
+            onClick={() => setModifyToggle(true)}
+            data-testid="modify-button"
+          >
+            수정
+          </button>
+          <button
+            onClick={() => deleteTodoList(item.id)}
+            data-testid="delete-button"
+          >
+            삭제
+          </button>
+        </li>
+      )}
+    </div>
   );
 }
 
