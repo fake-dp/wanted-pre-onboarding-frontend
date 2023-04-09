bd200edc59ecb3e741e42386d86545f0a575bae3
fake-dp <fakecoko92@gmail.com>
2023-04-09 14:47:56 +0900

    last test

--- client/src/api/instance.jsx
+++ client/src/api/instance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "https://pre-onboarding-selection-task.shop/",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
